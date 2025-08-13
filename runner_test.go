b53a8617b8f86449fe13b48c6779c2247e66a916
Matthew Rothenberg <mroth@mroth.xyz>
2025-08-13 15:51:18 -0400

    test: update to use synctest

--- runner_test.go
+++ runner_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"testing"
+	"testing/synctest"
 	"time"
 )
 
@@ -14,55 +15,54 @@ func NewRunnerWithExecutor(ctx context.Context, executor mockExecutor) *Runner {
 }
 
 func TestRunner_Run(t *testing.T) {
-	t.Parallel()
-
 	t.Run("simple success", func(t *testing.T) {
-		t.Parallel()
-		r := NewRunnerWithExecutor(t.Context(), mockExecutor{exitcode: 0})
+		synctest.Test(t, func(t *testing.T) {
+			r := NewRunnerWithExecutor(t.Context(), mockExecutor{exitcode: 0})
 
-		runAssert(t, r, runnerExpectedResults{
-			err:  nil,
-			runs: 1,
+			runAssert(t, r, runnerExpectedResults{
+				err:  nil,
+				runs: 1,
+			})
 		})
 	})
 
 	t.Run("fails when exceeds max runs", func(t *testing.T) {
 		t.Parallel()
 
-		r := NewRunnerWithExecutor(t.Context(), mockExecutor{exitcode: 1})
-		r.MaxRuns = 3
-		r.RetryDelay = 10 * time.Millisecond
+		synctest.Test(t, func(t *testing.T) {
+			r := NewRunnerWithExecutor(t.Context(), mockExecutor{exitcode: 1})
+			r.MaxRuns = 3
+			r.RetryDelay = 10 * time.Millisecond
 
-		runAssert(t, r, runnerExpectedResults{
-			err:          errMaxRunsCompleted,
-			runs:         3,
-			elapsedTotal: 30 * time.Millisecond,
+			runAssert(t, r, runnerExpectedResults{
+				err:          errMaxRunsCompleted,
+				runs:         3,
+				elapsedTotal: 30 * time.Millisecond,
+			})
 		})
 	})
 
 	t.Run("context deadline exceeded", func(t *testing.T) {
-		t.Parallel()
+		synctest.Test(t, func(t *testing.T) {
+			// set overall runner context timeout for 45ms,
+			// and have runner run a failing command every 10ms via retry delay.
+			ctx, cancel := context.WithTimeout(t.Context(), 45*time.Millisecond)
+			t.Cleanup(cancel)
+			r := NewRunnerWithExecutor(ctx, mockExecutor{exitcode: 1})
+			r.RetryDelay = 10 * time.Millisecond
 
-		// set overall runner context timeout for 50ms,
-		// and have runner run a failing command every ~10ms via retry delay.
-		ctx, cancel := context.WithTimeout(t.Context(), 50*time.Millisecond)
-		t.Cleanup(cancel)
-		r := NewRunnerWithExecutor(ctx, mockExecutor{exitcode: 1})
-		r.RetryDelay = 10 * time.Millisecond
-
-		runAssert(t, r, runnerExpectedResults{
-			err:          context.DeadlineExceeded,
-			runs:         5,
-			elapsedTotal: 50 * time.Millisecond,
+			runAssert(t, r, runnerExpectedResults{
+				err:          context.DeadlineExceeded,
+				runs:         5,
+				elapsedTotal: 45 * time.Millisecond,
+			})
 		})
 	})
 
 	t.Run("process timeout", func(t *testing.T) {
-		t.Parallel()
-
 		// Run a command that sleeps for 100ms before success, but with a process timeout of 50ms.
 		//
-		// We should see MaxRuns failures occur over a total ~150ms, as each process is canceled
+		// We should see MaxRuns failures occur over a total 150ms, as each process is canceled
 		// due to a deadline exceeded error before it completes.
 		var (
 			executorSleepDuration = 100 * time.Millisecond
@@ -73,14 +73,16 @@ func TestRunner_Run(t *testing.T) {
 			wantTotalElapsed      = time.Duration(runnerMaxRuns) * runnerProcessTimeout
 		)
 
-		r := NewRunnerWithExecutor(t.Context(), mockExecutor{sleep: executorSleepDuration})
-		r.ProcessTimeout = runnerProcessTimeout
-		r.MaxRuns = uint(runnerMaxRuns)
+		synctest.Test(t, func(t *testing.T) {
+			r := NewRunnerWithExecutor(t.Context(), mockExecutor{sleep: executorSleepDuration})
+			r.ProcessTimeout = runnerProcessTimeout
+			r.MaxRuns = uint(runnerMaxRuns)
 
-		runAssert(t, r, runnerExpectedResults{
-			err:          wantErr,
-			runs:         wantRunsCompleted,
-			elapsedTotal: wantTotalElapsed,
+			runAssert(t, r, runnerExpectedResults{
+				err:          wantErr,
+				runs:         wantRunsCompleted,
+				elapsedTotal: wantTotalElapsed,
+			})
 		})
 	})
 }
@@ -101,27 +103,13 @@ func runAssert(t *testing.T, r *Runner, want runnerExpectedResults) {
 		t.Errorf("runs completed: got %d, want %d", r.runsCompleted, want.runs)
 	}
 
-	assertDurationElapsed(t, elapsed, want.elapsedTotal)
+	if elapsed != want.elapsedTotal {
+		t.Errorf("Expected duration %v, got %v", want.elapsedTotal, elapsed)
+	}
 }
 
 type runnerExpectedResults struct {
 	err          error
 	runs         uint
 	elapsedTotal time.Duration
-}
-
-// "Rough" duration elapsed equality, allowing for a small margin of error.
-//
-// We do this by making sure that the elapsed time is greater than or equal to
-// the expected duration, and that the difference between the elapsed and
-// the expected duration is within a small margin of reasonable runtime overhead.
-//
-// This will no longer be necessary once using synctest bubbles in go1.25.
-func assertDurationElapsed(t *testing.T, got, want time.Duration) {
-	t.Helper()
-
-	overhead := 10 * time.Millisecond
-	if got < want || got > want+overhead {
-		t.Errorf("Expected duration ~>=%v, got %v", want, got)
-	}
 }
